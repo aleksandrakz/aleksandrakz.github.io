a21e8a70315b9b2908fd5a9675f78e9dc5eaa9f3
Aleksandra Zakrzewska <aleksandra.zakrzewska@yale.edu>
2016-05-03 21:21:58 -0400

    idk

--- js/scripts.js
+++ js/scripts.js
@@ -5,22 +5,16 @@ $(document).ready(function(){
 	    var myIFrame = document.getElementById('pageframe');
          var myIFDoc  = myIFrame.contentWindow.document;
          var button = myIFDoc.getElementsByName("first");
+         console.log(window.frames);
+         console.log(myIFDoc);
+         console.log(button);
 
     $(button).click(function(){
     	// $("#pageframe").animate({left: '500px'});
+    	alert("clicked");
 
          $(button).hide();
-    	 $("#pageframe1").contents().find("#first").hide();
-    	 $("#pageframe1").animate({float:"center", "top":"-700px", "left":"100px"})
-    	 $("#pageframe1").show();
-    	 $("#pageframe").animate({
-	    	width: "60%",
-		    opacity: 0.8,
-		    marginLeft: "-50%",
-		    borderWidth: "10px",
-		    //"left": "-=200px"
-		  }, 1500 );
-		});
+    	 });
 
         $("#pageframe1").click(function(){
     	 $("#pageframe1").animate({left: '500px'});
@@ -75,4 +69,18 @@ window.onload = function() {
     jPM.on();
  
 };
+
+$( document ).keypress(function(event) {
+  if ( event.which == 97 ) {
+    	 $("#pageframe1").animate({float:"center", "top":"-700px", "left":"100px"})
+    	 $("#pageframe1").show();
+    	 $("#pageframe").animate({
+	    	width: "60%",
+		    opacity: 0.8,
+		    marginLeft: "-50%",
+		    borderWidth: "10px",
+		    //"left": "-=200px"
+		  }, 1500 );
+    	}
+});
 
