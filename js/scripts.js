37d71c3815e84a164eec6e3567a8061b84892d8f
Aleksandra Zakrzewska <aleksandra.zakrzewska@yale.edu>
2016-05-04 00:45:53 -0400

    jason helped get cclicks--now need to make menu actually work

--- js/scripts.js
+++ js/scripts.js
@@ -1,74 +1,110 @@
 $(document).ready(function(){
 	$("#pageframe1").hide();
 	$("#pageframe2").hide();
+	$("#back1").hide();
 
-	    var myIFrame = document.getElementById('pageframe');
-         var myIFDoc  = myIFrame.contentWindow.document;
-         var button = myIFDoc.getElementsByName("first");
-         console.log(window.frames);
-         console.log(myIFDoc);
-         console.log(button);
-
-    $(button).click(function(){
-    	// $("#pageframe").animate({left: '500px'});
-    	alert("clicked");
-
-         $(button).hide();
-    	 });
-
-        $("#pageframe1").click(function(){
-    	 $("#pageframe1").animate({left: '500px'});
-    	 $("#pageframe2").show();
-    	 $("#pageframe").hide();
-    	 $("#pageframe1").animate({
+  	$("#click").click(function(){
+    	 //$("#pageframe").animate({left: '500px'});
+    	console.log("in");
+    	 $("#pageframe").animate({
 	    	 width: "60%",
 		    opacity: 0.8,
-		    marginLeft: "-50%",
-		    fontSize: "3em",
+		    marginLeft: "-30%",
 		    borderWidth: "10px",
-		    "left": "-=200px"
+		    "left": "-200px"
+		  }, 1500 );
+    	  $("#pageframe1").show("slow");
+    	  $("#back1").show();
+		});
+
+  	$("#back1").click(function(){
+    	 //$("#pageframe").animate({left: '500px'});
+    	console.log("out");
+    	 $("#pageframe").animate({
+	    	position:"absolute",
+	    	width: "80%",
+		    opacity: 1.0, 
+    		height: "600px",
+		    "top": "100px",
+		    "marginLeft": "10%",
+		    left:"0%",
+		  }, 1500 );
+
+    	  $("#pageframe1").hide("slow");
+    	  $("#back1").hide();
+    	});
+  	$("#back2").click(function(){
+    	 //$("#pageframe").animate({left: '500px'});
+    	 $("#pageframe1").animate({
+	    	position:"absolute",
+	    	width: "80%",
+		    opacity: 1.0, 
+    		height: "600px",
+		    "top": "100px",
+		    "marginLeft": "10%",
+		    left:"0%",
+		  }, 1500 );
+
+    	  $("#pageframe2").hide("slow");
+    	  $("#back2").hide();
+    	  $("#pageframe").show();
+    }); 
+
+    $("#click2").click(function(){
+    	 //$("#pageframe1").animate({left: '500px'});
+    	 $("#pageframe2").show("slow");
+    	 $("#pageframe").hide();
+    	 $("#pageframe1").animate({
+	    	width: "60%",
+		    opacity: 0.8,
+		    marginLeft: "-30%",
+		    borderWidth: "10px",
+		    "left": "-200px"
 		  }, 1500 );
 		});
 
      $("#home").click(function(){
     	$("#pageframe1").hide();
     	$("#pageframe2").hide();
+    	$("#back1").hide();
     	$("#pageframe").show();
-    	$(pageframe).animate({
-	    	 width: "80%",
-		    opacity: 1.0,
-		    marginLeft: "10%",
-		    fontSize: "3em",
-		    borderWidth: "10px",
-		    "left": "-=200px"
+    	$("#pageframe").animate({
+    		position:"absolute",
+	    	width: "80%",
+		    opacity: 1.0, 
+    		height: "600px",
+		    "top": "100px",
+		    "marginLeft": "10%",
+		    left:"0%",
 		  }, 1500 );
+    	
     	$("#pageframe1").animate({
-    		float:"center",
-    		opacity: 1.0,
-    		"margin-top": "100px",
-		    "margin-bottom": "0px",
-		    "margin-left": "50px",
-		    "margin-right":"0px"
+    		width: "80%",
+		    opacity: 1.0, 
+    		height: "600px",
+		    "top": "100px",
+		    "marginLeft": "10%",
+		    left:"0%",
+		  }, 1500 );
 		})
 		$("#pageframe2").animate({
-			float:"center",
-			opacity: 1.0,
-    		"margin-top": "100px",
-		    "margin-bottom": "0px",
-		    "margin-left": "50px",
-		    "margin-right":"0px"
-		})
-    });
+				width: "80%",
+		    opacity: 1.0, 
+    		height: "600px",
+		    "top": "100px",
+		    "marginLeft": "10%",
+		    left:"0%",
+		  }, 1500 );
 });
 
-window.onload = function() {
+/*window.onload = function() {
     var jPM = $.jPanelMenu({
 	    menu: '#sidebar',
 	    trigger: '#menuon'
 	});
     jPM.on();
  
-};
+};*/
 
 $( document ).keypress(function(event) {
   if ( event.which == 97 ) {
